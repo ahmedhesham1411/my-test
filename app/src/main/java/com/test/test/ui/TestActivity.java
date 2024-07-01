3aa6932fb762d9d718e1f490a9e638e40e22e1be
Ahmed Aly <a.mostafa@ebetech.com.eg>
2024-07-01 11:57:58 +0300

    myCommit

---
+++ app/src/main/java/com/test/test/ui/TestActivity.java
@@ -0,0 +1,16 @@
+package com.test.test.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.test.test.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
